e3d022fca3f2b6ef5385d80abf5af6d52bbb30a0
thanujsai <sboda@modeln.com>
2024-06-07 18:34:03 +0530

    working

--- src/App.js
+++ src/App.js
@@ -2,49 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 import Template from './Components/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import { Button } from 'antd';
 
 function App() {
+  const [counts, setCounts] = useState(Array(9).fill(0));
+  const [sums, setSums] = useState(Array(9).fill(0));
+
+  const resetCountsAndSums = () => {
+    setCounts(Array(9).fill(0));
+    setSums(Array(9).fill(0));
+  };
+
+  const incrementCount = (index, a) => {
+    const newCounts = [...counts];
+    newCounts[index] += 1;
+    setCounts(newCounts);
+
+    const newSums = [...sums];
+    newSums[index] = newCounts[index] * a;
+    setSums(newSums);
+  };
+
   return (
     <div className="App">
       <h1>Thanuj</h1>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <Template a={1} />
-          </div>
-          <div className="col-md-4">
-            <Template a={2} />
-          </div>
-          <div className="col-md-4">
-            <Template a={3} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <Template a={4} />
-          </div>
-          <div className="col-md-4">
-            <Template a={5} />
-          </div>
-          <div className="col-md-4">
-            <Template a={6} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <Template a={7} />
-          </div>
-          <div className="col-md-4">
-            <Template a={8} />
-          </div>
-          <div className="col-md-4">
-            <Template a={9} />
-          </div>
+          {Array.from({ length: 9 }).map((_, index) => (
+            <div className="col-md-4" key={index}>
+              <Template
+                a={index + 1}
+                count={counts[index]}
+                sum={sums[index]}
+                increment={() => incrementCount(index, index + 1)}
+              />
+            </div>
+          ))}
         </div>
       </div>
-      <h3>Total Sum :</h3>
-      <Button danger className='btn'>Reset</Button>   
+      <h3>Total Sum: {sums.reduce((acc, curr) => acc + curr, 0)}</h3>
+      <Button danger className="btn" onClick={resetCountsAndSums}>Reset</Button>
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,28 +1,14 @@
-import { Button, Flex, Card } from 'antd';
-import useSelection from 'antd/es/table/hooks/useSelection';
-import { useState } from 'react';
+import { Button, Card } from 'antd';
+import { useEffect } from 'react';
 
-function Template({a}) {
-
-    console.log("a is "+a)
-    const [sum,setSum] = useState(0);
-    const [count,setCount] = useState(0);
-
-    const increment = () => {
-        summ();
-        setCount(count+1)
-    }
-    const summ = () => {
-        setSum(count*a)
-    }
+function Template({ a, count, sum, increment }) {
   return (
-      <div className="card-wrap">
-      <Card className='Card'>
-        Click Count : {count}<hr></hr>
-        Sum : {sum}
+    <div className="card-wrap">
+      <Card className="Card">
+        Click Count: {count}<hr />
         <Button type="primary" onClick={increment}>{a}</Button>
       </Card>
-      </div>
+    </div>
   );
 }
 
